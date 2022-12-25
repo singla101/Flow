e9da3f8441c94b39d94b42675aaad8daf0001ef9
shiva <gautamsingla571@gmail.com>
2022-12-25 21:04:48 +0530

    iouuyiyh

---
+++ app/src/main/java/com/gwtf/flow/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.gwtf.flow
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.gwtf.flow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
