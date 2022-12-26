e4c193642426e912b4effff42eecd27fd19cae81
Tanisha345 <120730408+Tanisha345@users.noreply.github.com>
2022-12-26 11:19:20 +0530

    Update and rename SignupActivity.kt to SignupActivity.java

---
+++ app/src/main/java/com/gwtf/flow/SignupActivity.java
@@ -0,0 +1,92 @@
+package com.gwtf.phoneauthentication;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.gwtf.phoneauthentication.databinding.ActivityMainBinding;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignupActivity extends AppCompatActivity {
+private ActivitySignupBinding binding;
+private FirebaseAuth mauth;
+private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mauth=FirebaseAuth.getInstance();
+        binding.btnsendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.contact.getText().toString().trim().isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                else if(binding.contact.getText().toString().trim().length()!=10){
+                    Toast.makeText(SignupActivity.this, "Please enter valid phone number", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    otpSend();
+                }
+            }
+        });
+
+            }
+
+    private void otpSend() {
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+                Toast.makeText(SignupActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+
+                Intent intent =new Intent(SignupActivity.this,VerifyotpActivity.class);
+                intent.putExtra("phone",binding.contact.getText().toString().trim());
+                intent.putExtra("verificationId",verificationId);
+                startActivity(intent);
+
+            }
+        };
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mauth)
+                        .setPhoneNumber("+91"+binding.contact.getText().toString().trim())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+
+}
