4c8802ffa63c67db26aa381f482aba2139be23ed
Tanisha345 <120730408+Tanisha345@users.noreply.github.com>
2022-12-26 11:14:37 +0530

    Update MainActivity.kt

--- app/src/main/java/com/gwtf/flow/MainActivity.kt
+++ app/src/main/java/com/gwtf/flow/MainActivity.kt
@@ -1,23 +1,92 @@
-package com.gwtf.flow
+package com.gwtf.phoneauthentication;
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.gwtf.flow.databinding.ActivityMainBinding
+import static android.content.ContentValues.TAG;
 
-class MainActivity : AppCompatActivity() {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.gwtf.phoneauthentication.databinding.ActivityMainBinding;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
+private FirebaseAuth mauth;
+private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mauth=FirebaseAuth.getInstance();
+        binding.btnsendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.contact.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                else if(binding.contact.getText().toString().trim().length()!=10){
+                    Toast.makeText(MainActivity.this, "Please enter valid phone number", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    otpSend();
+                }
+            }
+        });
+
+            }
+
+    private void otpSend() {
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+
+                Intent intent =new Intent(MainActivity.this,Otpactivity.class);
+                intent.putExtra("phone",binding.contact.getText().toString().trim());
+                intent.putExtra("verificationId",verificationId);
+                startActivity(intent);
+
+            }
+        };
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mauth)
+                        .setPhoneNumber("+91"+binding.contact.getText().toString().trim())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
+
 
 }
